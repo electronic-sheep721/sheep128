df03f6e84a53bde318c3dc021adaba40e4fa8b5d
electronic-sheep721 <3253902326@qq.com>
2025-01-27 19:55:35 +0800

    happybirthday

---
+++ videojs/index.js
@@ -0,0 +1,29 @@
+const core = window._PlayerCore;
+
+core.AppendSong({
+  name: "封茗囧菌 - 我的悲伤是水做的",
+  id: 0,
+  src: "./videojs/封茗囧菌 - 我的悲伤是水做的.mp3",
+  img: "https://tse3-mm.cn.bing.net/th/id/OIP-C.lzMUkBAcvVizfUPqfgoyrQHaHa?w=172&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+});
+
+core.AppendSong({
+  name: "Normal No More(哈基米)",
+  id: 1,
+  src: "./videojs/还给我神ID - Normal No More(哈基米).mp3",
+  img: "http://p1.music.126.net/xeBSNi9X6ulMrCwZSwSqvw==/109951170119141954.jpg?param=130y130",
+});
+
+core.AppendSong({
+  name: "2：23 AM(哈基米)",
+  id: 2,
+  src: "./videojs/还给我神ID - 2：23 AM(哈基米).mp3",
+  img: "http://p1.music.126.net/dpV89bjLLKMhSWdNmcm9Uw==/109951170386932767.jpg?param=130y130",
+});
+
+core.AppendSong({
+  name: "哈基米的开心一天",
+  id: 3,
+  src: "./videojs/akulamatata - 曼波、那咩、有时哈基米.mp3",
+  img: "http://p2.music.126.net/aMYutnVkwQvNYAT6pfl5lA==/109951169775091160.jpg?param=130y130",
+});
